2c0a6d99eca2aebbec6ce7e92033ebbe1f7db178
Quentin Prigent <qp.prigent@gmail.com>
2017-12-15 17:11:04 +0100

    Commit toto

--- src/app/profil/containers/profil/profil.component.ts
+++ src/app/profil/containers/profil/profil.component.ts
@@ -34,28 +34,4 @@ export class ProfilComponent implements OnInit {
     console.log(this.skills);
   }
 
-
-/*   this.dao.getUsers()
-  .subscribe(
-    data => data.forEach(elt => this.users = data),
-    err => console.error(`Erreur: `, err)
-  ); */
-
-/*   this.dao.getUsers()
-    .subscribe(
-      data => data.forEach(elt => console.log(elt)),
-      err => console.error(`Erreur: `, err)
-    ); */
-
-/*   addUser() {
-    const params = {
-      'firstname':	'pierre',
-      'lastname':	'ned',
-      'email': 'pierre.ned@capgemini.com',
-      'password': 'jaimebnpparibas'
-    }; */
-        // console.log(params);
-        // this.service.addUser(params).subscribe((data) => console.log(data));
-      }
-
-
+}
