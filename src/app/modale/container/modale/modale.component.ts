925ff2e79bd0943cf2943cd182cea3290e15827d
Jules TABERLY <jules.taberly@imie.lan>
2017-12-18 09:14:16 +0100

    anngoularr

---
+++ src/app/modale/container/modale/modale.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'skills-modale',
+  templateUrl: './modale.component.html',
+  styleUrls: ['./modale.component.css']
+})
+export class ModaleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modale/modale.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModaleComponent } from './container/modale/modale.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ModaleComponent]
+})
+export class ModaleModule { }
